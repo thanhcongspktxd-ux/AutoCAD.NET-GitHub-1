6a760df16a27c367021dd7f3619463724b6fd924
Công Nguyễn <thanhcongspktxd@gmail.com>
2026-06-09 21:08:38 +0700

    Add project files.

---
+++ AutoCAD.NET GitHub 1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.Colors;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Internal;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Runtime.ConstrainedExecution;
+
+namespace AutoCAD.NET_GitHub_1
+{
+    public class B1
+    {
+    }
+}
